088bdcb79f0408f999e8414a0346074930e26520
giraffecat <1018997102@qq.com>
2017-06-04 10:55:36 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
-module.exports = function main() {
-  var str='123';
+module.exports = function main(str) {
+  var str=this.str;
   var sum=parseInt(str[0])+parseInt(str[1])+parseInt(str[2]);
   return sum; 
 };
